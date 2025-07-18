77c4d8156b6c2fdbfd99b5768ff5facac4781351
ادریس پاپی <64495523+edrispapi@users.noreply.github.com>
2025-07-19 00:00:01 +0330

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import LLMQuerySerializer
 from services.llm_query import llm_query
+from utils.logger import log_request
 
 class LLMQueryAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        question = request.data.get('question', '')
-        if not question:
-            return Response({'error': 'Question is required'}, status=400)
+        serializer = LLMQuerySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = request.user
+        question = serializer.validated_data['question']
         answer = llm_query(question)
+        log_request(user, '/api/ask-llm/', request.data, {'answer': answer}, 200)
         return Response({'answer': answer})

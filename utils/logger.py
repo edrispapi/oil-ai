1fd0e928e79c18f9f96ad841c5414f1782c8e05e
ادریس پاپی <64495523+edrispapi@users.noreply.github.com>
2025-07-19 00:41:57 +0330

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,26 +1,21 @@
 from models.logs import APILog
 from kafka import KafkaProducer
-import os, json, traceback
+import json
 
 producer = KafkaProducer(
     bootstrap_servers=['kafka:9092'],
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
-def log_operation(event, data):
+def log_operation(event, data, status_code=200):
     try:
         APILog.objects.create(
+            event=event,
             user_id=data.get("user_id"),
-            endpoint=event,
             request_data=data,
             response_data={},
-            status_code=200,
+            status_code=status_code,
         )
         producer.send('system-logs', {'event': event, 'data': data})
     except Exception as e:
-        print("Logging error:", str(e))
-
-def log_exception(component, err_message):
-    trace = traceback.format_exc()
-    producer.send('system-logs', {'event': 'exception', 'component': component, 'trace': trace, 'error': err_message})
-    # In production, optionally push to alerting systems, Sentry etc.
+        print("Logging error:", e)

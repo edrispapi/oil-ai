39348ab8f75be75cb77cee9950fc50bd1dd2fd60
ادریس پاپی <64495523+edrispapi@users.noreply.github.com>
2025-07-19 00:41:00 +0330

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,18 +1,15 @@
+# api/views.py
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import LLMQuerySerializer
-from services.llm_query import llm_query
-from utils.logger import log_request
+from rest_framework.response import Response
+from .serializers import SensorQuerySerializer
+from services.rag_pipeline import answer_with_rag
 
-class LLMQueryAPIView(APIView):
+class SensorQueryAPIView(APIView):
     permission_classes = [IsAuthenticated]
-
     def post(self, request):
-        serializer = LLMQuerySerializer(data=request.data)
+        serializer = SensorQuerySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = request.user
-        question = serializer.validated_data['question']
-        answer = llm_query(question)
-        log_request(user, '/api/ask-llm/', request.data, {'answer': answer}, 200)
-        return Response({'answer': answer})
+        question = serializer.validated_data["question"]
+        answer = answer_with_rag(question, user_id=request.user.id)
+        return Response({"answer": answer})
